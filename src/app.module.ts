b5e184f16104767ac054e62f450a1b9727e8475e
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-07-10 03:14:53 +0530

    chore: fix typo in ConfigModule global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGloble : true,
+      isGlobal : true,
     })
   ],
  
