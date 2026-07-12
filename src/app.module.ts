ca5b306f41a8d2d28f8ae8abd99466014dd1575d
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-07-12 18:47:27 +0530

    feat: replace DatabaseModule with inline TypeOrmModule config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServiceModule } from './api/service/service.module';
 
 @Module({
   imports: [
@@ -8,7 +9,20 @@ import { DatabaseModule } from './database/database.module';
       isGlobal: true,
     }),
 
-    DatabaseModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.booking_db,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ServiceModule,
+
+
+
   ],
 })
 export class AppModule {}
