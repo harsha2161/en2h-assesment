f7e1e57a10658073b44ea85a93f196113cb37df6
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-07-12 19:55:39 +0530

    refactor:  database configuration into a separate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServiceModule } from './api/service/service.module';
+import { DatabaseModule } from './db/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.booking_db,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-
+    DatabaseModule,
     ServiceModule,
-
-
-
   ],
 })
 export class AppModule {}
---
+++ src/db/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('booking_db'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
