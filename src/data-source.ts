4e667fb6b60f81a1376c1ff86e964617fb0f863e
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-07-13 12:13:14 +0530

    add: added data-source file

---
+++ src/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || '1234',
+  database: process.env.DB_DATABASE || 'booking_db',
+  synchronize: false,
+  logging: false,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+  subscribers: [],
+});
